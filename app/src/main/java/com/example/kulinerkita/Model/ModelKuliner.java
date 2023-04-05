da5ea7c26b61bd82c6cb15388d7929bf61b270d3
Rikky250503 <rikky25@mhs.mdp.ac.id>
2023-04-05 09:18:53 +0700

    kulinerKita

---
+++ app/src/main/java/com/example/kulinerkita/Model/ModelKuliner.java
@@ -0,0 +1,21 @@
+package com.example.kulinerkita.Model;
+
+public class ModelKuliner {
+    private int id, nama, asal, deskripsi_singkat;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNama() {
+        return nama;
+    }
+
+    public int getAsal() {
+        return asal;
+    }
+
+    public int getDeskripsi_singkat() {
+        return deskripsi_singkat;
+    }
+}
