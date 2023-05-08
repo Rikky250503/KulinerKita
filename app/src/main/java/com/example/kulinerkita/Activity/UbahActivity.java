d8d2df73ff7a3ae3274d907f406132fd261a7e1d
Rikky250503 <rikky25@mhs.mdp.ac.id>
2023-05-08 14:31:08 +0700

    update activity main

---
+++ app/src/main/java/com/example/kulinerkita/Activity/UbahActivity.java
@@ -0,0 +1,16 @@
+package com.example.kulinerkita.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kulinerkita.R;
+
+public class UbahActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah);
+    }
+}
